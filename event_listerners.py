9b513319c5ff2ec0bd6f7d23373e0390d623e8ce
Pratyush Rao <pratyush.rao@theopeneyes.in>
2024-10-09 17:38:07 +0530

    Added files to get started

---
+++ event_listerners.py
@@ -0,0 +1,34 @@
+# This file contains the event listener addition class
+
+#TODO: Implement event listener to logs things into a db 
+# for help read this link: https://stackoverflow.com/questions/70982565/how-do-i-make-an-event-listener-with-decorators-in-python/74679391#74679391
+
+class Event:
+    def __init__(self):
+        # Initialise a list of listeners
+        self.__listeners = []
+    
+    # Define a getter for the 'on' property which returns the decorator.
+    @property
+    def on(self):
+        # A declorator to run addListener on the input function.
+        def wrapper(func):
+            self.addListener(func)
+            return func
+        return wrapper
+    
+    # Add and remove functions from the list of listeners.
+    def addListener(self,func):
+        if func in self.__listeners: return
+        self.__listeners.append(func)
+    
+    def removeListener(self,func):
+        if func not in self.__listeners: return
+        self.__listeners.remove(func)
+    
+    # Trigger events.
+    def trigger(self,args = None):
+        # Run all the functions that are saved.
+        if args is None:
+            args = []
+        for func in self.__listeners: func(*args)
---
+++ main.py
@@ -0,0 +1,43 @@
+# this file will contain all the fastapi endpoints 
+from fastapi import FastAPI 
+from dotenv import load_dotenv # for the purposes of loading hidden environment variables
+from prompt_design import prompts_csv # to get the collection of prompts  
+from huggingface_hub import login 
+from transformers import pipeline 
+from transformers.pipelines.base import Pipeline 
+
+import models as md
+import pandas as pd 
+
+import os 
+
+# loads the variables in the .env file 
+load_dotenv()
+
+# environment variables: configured in .env file
+# these variables will be instantiated once the server starts and 
+# the value won't be updated until you restart the server 
+PROMPT_FILE_ID: str = os.getenv("FILE_ID", None) # file_id to fetch remote prompt design sheet
+HF_TOKEN: str = os.getenv("HF_TOKEN", None)
+MODEL: str = os.getenv("MODEL", None)
+
+# model pipeline
+pipe: Pipeline = pipeline(
+    "text-generation", 
+    md.SLM_MODELS[MODEL], 
+    max_new_tokens=256, 
+)
+
+# prompt_design csv: 
+PROMPT_DESIGN: pd.DataFrame = prompts_csv(PROMPT_FILE_ID) 
+
+# logging in to huggingface with fine-grained token
+login(token=HF_TOKEN)
+
+# testing phase therefore `debug=True`
+app = FastAPI(debug=True, title="project-astra")
+
+#Endpoints 
+@app.post("/get_classification")
+def get_classification(hashcode: str) -> pd.DataFrame: 
+    pass  
