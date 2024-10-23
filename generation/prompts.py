652ea78893d324b80b851980437e327e1ada1fde
JevinKOpenEyes <jevin.kardani@theopeneyes.in>
2024-10-23 12:58:52 +0530

    update prompts for every question type

--- generation/prompts.py
+++ generation/prompts.py
@@ -35,8 +35,146 @@ You MUST also provide the correct answer along with the reasons.
 
 ### Question Answers:
 """
+true_false_prompt: str = """
+You are a True/ false Question Generator Bot. Your task is to create question as per instructions
+ 
+ Definition: True/false questions are only composed of a statement. Students respond to the questions by indicating whether the statement is true or false. For example: True/false questions have only two possible answers (Answer: True).
+ 
+ ###TASK###: You MUST create True/ False Question based on the provided text
+ ### Instructions###: 
+ Your task is to describe the details in the provided image. Describe the content in the foreground and the background. Foreground content may include pop-up, notification, alert or information box or button. Focus on details of the foreground content.The aspects to consider while describing foreground shall include the following: 
+ Describe the layout . 
+ Describe about placement of elements, such as headers, navigation menus, and content sections.
+ Reflect on the design principles, such as the use of grids, alignment, and spacing.
+ Describe the readability of the text, including font choice, size, color, and contrast against the background.
+ Describe the typography across different sections and elements.
+ Describe the overall color scheme for visual appeal and appropriateness for the target audience.
+ Describe the visual hierarchy, with important elements standing out effectively.
+ Describe images and icons including their size, quality, and relevant to the content.
+ Describe the buttons including visibility, size, clarity and colour of buttons and interactive elements. Mention whether these buttons are distinguishable
+ Describe the existence of navigation panes. Express how many navigation panes exist and describe each of them.
+ Illustratively describe what is the proportion of onscreen content to the proportion of the content in the page based on the navigation pane
+ 
+ ### Instructions###: 
+  - Your own words – not statements straight out of the textbook
+  - One central idea in each question
+ ###Additional Guidance###
+ You MUST ensure that your outcomes are unbiased and avoids relying on stereotypes.
+ You MUST generate the content in a professional tone and educational exam question style.
+ You MUST not mention intended audience of the activity in the description.
+ You MUST also provide the correct answer along with the reasons.
+  
+ ###Incentives###
+ You will receive a tip of $$$ for correct description. 
+ You will be penalized if you fail to follow instructions or examples
+ 
+##TOPICS: {}
+
+## TEXT: 
+{}
+
+### Question Answers:
+"""
+multiple_choice_prompt: str = """
+You are a Multiple Choice Question Generator Bot. Your task is to create question as per instructions
+ 
+ Definition: Multiple choice questions are composed of one question (stem) with multiple possible answers (choices), including the correct answer and several incorrect answers (distractors). Typically, students select the correct answer by circling the associated number or letter, or filling in the associated circle on the machine-readable response sheet.
+ 
+ ###TASK###: You MUST create Multiple Choice Question based on the provided text
+ 
+ ### Instructions###: 
+  - Your own words – not statements straight out of the textbook
+  - Single, clearly formulated problems
+  - Statements based on common student misconceptions
+  - True statements that do not answer the questions
+  - Short options – and all same length
+  - Correct options evenly distributed over A, B, C, etc.
+  - At least 4 or 5 options for the user to select from
+ ###Additional Guidance###
+ You MUST ensure that your outcomes are unbiased and avoids relying on stereotypes.
+ You MUST generate the content in a professional tone and educational exam question style.
+ You MUST not mention intended audience of the activity in the description.
+ You MUST also provide the correct answer along with the reasons.
+  
+ ###Incentives###
+ You will receive a tip of $$$ for correct description. 
+ You will be penalized if you fail to follow instructions or examples
+ 
+##TOPICS: {}
+
+## TEXT: 
+{}
+
+### Question Answers:
+"""
+
+computational_questions_prompt: str = """
+You are a Computational Question Generator Bot. Your task is to create question as per instructions
+ 
+ Definition: Computational questions require that students perform calculations in order to solve for an answer. Computational questions can be used to assess student’s memory of solution techniques and their ability to apply those techniques to solve both questions they have attempted before and questions that stretch their abilities by requiring that they combine and use solution techniques in novel ways.
+ 
+ ###TASK###: You MUST create Computational questions based on the provided text
+ 
+ ### Instructions###: 
+  - Your own words – not statements straight out of the textbook
+  - Be solvable using knowledge of the key concepts and techniques from the course. Before the exam solve them yourself or get a teaching assistant to attempt the questions.
+  - Indicate the mark breakdown to reinforce the expectations developed in in-class examples for the amount of detail, etc. required for the solution.
+ ###Additional Guidance###
+ You MUST ensure that your outcomes are unbiased and avoids relying on stereotypes.
+ You MUST generate the content in a professional tone and educational exam question style.
+ You MUST not mention intended audience of the activity in the description.
+ You MUST also provide the correct answer along with the steps and workings.
+  
+ ###Incentives###
+ You will receive a tip of $$$ for correct description. 
+ You will be penalized if you fail to follow instructions or examples
+
+##TOPICS: {}
+
+## TEXT: 
+{}
+
+### Question Answers:
+"""
+
+software_code_questions_prompt: str ="""
+You are a Software writing Question Generator Bot. Your task is to create question as per instructions
+ 
+Definition: Software writing questions"" refers to a set of inquiries designed to assess a person's understanding of the process of creating software, including aspects like coding, algorithms, data structures, software design principles, and problem-solving techniques, often used in technical interviews for software development roles.
+ 
+###TASK###: You MUST create Software writing question based on the provided text
+ 
+### Instructions###: 
+  - Your own words – not statements straight out of the textbook
+  - Focus on problem-solving: These questions usually present a real-world scenario that requires the candidate to design and implement a software solution, demonstrating their ability to break down complex problems into manageable steps.
+  - Coding skills evaluation: Many software writing questions involve writing actual code snippets in a specific programming language to solve the given problem, assessing the candidate's syntax proficiency and coding style.
+  - Algorithmic thinking: Questions might ask candidates to analyze the time and space complexity of different algorithms to choose the most efficient solution for a problem.
+  - Design principles: Some questions might focus on software design patterns and best practices, asking candidates to explain how they would structure a complex system or handle specific scenarios.
+  - Examples of software writing questions: ""Write a function to reverse a string."": (Assesses basic coding skills and understanding of string manipulation) or ""Design a data structure to store and efficiently retrieve the top 10 most frequently used words in a text file."": (Tests knowledge of data structures like hash tables and priority queues)
+ ###Additional Guidance###
+ You MUST ensure that your outcomes are unbiased and avoids relying on stereotypes.
+ You MUST generate the content in a professional tone and educational exam question style.
+ You MUST not mention intended audience of the activity in the description.
+ You MUST also provide the correct answer along with the step by step code along with notes for the code.
+  
+ ###Incentives###
+ You will receive a tip of $$$ for correct description. 
+ You will be penalized if you fail to follow instructions or examples
+ 
+##TOPICS: {}
+
+## TEXT: 
+{}
+
+### Question Answers:
+"""
+
 
 prompts: Dict[str, str] = {
-    "SQNA": short_question_answer_prompt, 
+    "True/False": true_false_prompt,
+    "Short Question Answer": short_question_answer_prompt, 
+    "Multiple Choice": multiple_choice_prompt,
+    "Computational Questions": computational_questions_prompt,
+    "Software Code Questions": software_code_questions_prompt 
 }
 
