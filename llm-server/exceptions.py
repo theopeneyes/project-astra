817270eae736d36fb81d18567d4d1a94ac5e7cdd
JevinKOpenEyes <jevin.kardani@theopeneyes.in>
2025-01-08 11:33:21 +0530

    Made changes in exceptions

--- llm-server/exceptions.py
+++ llm-server/exceptions.py
@@ -1,3 +1,15 @@
 class EmptyPDFException(Exception):
     def __init__(self):
         super().__init__("Your PDF is empty. Upload a non-empty PDF, you noob")
+
+class CorruptPDFException(Exception):
+    def __init__(self):
+        super().__init__("The PDF you uploaded is corrupt.")
+
+class UnsupportedPDFException(Exception):
+    def __init__(self):
+        pass
+
+class IncorrectGCPBucketException(Exception):
+    def __init__(self):
+        super().__init__("The PDF was uploaded in an incorrect storage bucket. Please re-upload")
