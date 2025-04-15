a942e00582cecc40efc0aa310e279353c023186f
JevinKOpenEyes <jevin.kardani@theopeneyes.in>
2025-04-15 17:03:00 +0530

    Added error chan comment

--- autopipeline/modify.go
+++ autopipeline/modify.go
@@ -1,134 +1,134 @@
-package main 
+package main
 
 import (
-	"sync" 
-	"net/http" 
-	"fmt" 
-	"encoding/json" 
-	"bytes" 
-) 
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"sync"
+)
 
 type ErrorNode struct {
-	Err 		error 
-	Request 	*http.Request
-} 
+	Err     error
+	Request *http.Request
+}
 
 type ErrorChan struct {
-	mu 		sync.Mutex 
-	Errors 		[] ErrorNode  
-} 
+	mu     sync.Mutex
+	Errors []ErrorNode
+}
 
+// ErrorChan is a thread-safe channel for errors(basically an error channel.)
 func (ec *ErrorChan) AddErrorNode(err ErrorNode) {
-	ec.mu.Lock(); 
-	defer ec.mu.Unlock(); 
-	ec.Errors = append(ec.Errors, err); 
-} 
+	ec.mu.Lock()
+	defer ec.mu.Unlock()
+	ec.Errors = append(ec.Errors, err)
+}
 
 type ModifyRequestModel struct {
-	FileName 	string 	`json:"filename"`
-	EmailId 	string 	`json:"email_id"`	
-	BranchName 	string 	`json:"branch_name"`
-} 
+	FileName   string `json:"filename"`
+	EmailId    string `json:"email_id"`
+	BranchName string `json:"branch_name"`
+}
 
-func (pm *ProcessMetadata) Modify( url string ) ErrorChan {
-	var errChan ErrorChan; 
-	client := &http.Client{}; 
-	topicJson := ModifyRequestModel {
-		FileName  : 	pm.FileName, 
-		EmailId   : 	pm.EmailId, 
-		BranchName: 	"topic",  
-	} 
+func (pm *ProcessMetadata) Modify(url string) ErrorChan {
+	var errChan ErrorChan
+	client := &http.Client{}
+	topicJson := ModifyRequestModel{
+		FileName:   pm.FileName,
+		EmailId:    pm.EmailId,
+		BranchName: "topic",
+	}
 
+	conceptJson := ModifyRequestModel{
+		FileName:   pm.FileName,
+		EmailId:    pm.EmailId,
+		BranchName: "concept",
+	}
 
-	conceptJson := ModifyRequestModel {
-		FileName  : 	pm.FileName, 
-		EmailId   : 	pm.EmailId, 
-		BranchName: 	"concept",  
-	} 
+	headingJson := ModifyRequestModel{
+		FileName:   pm.FileName,
+		EmailId:    pm.EmailId,
+		BranchName: "heading_text",
+	}
 
-	headingJson := ModifyRequestModel {
-		FileName  : 	pm.FileName, 
-		EmailId   : 	pm.EmailId, 
-		BranchName: 	"heading_text",  
-	} 
-
-	topicBuf, err := json.Marshal(topicJson); 
+	topicBuf, err := json.Marshal(topicJson)
 	if err != nil {
-		errChan.Errors = append(errChan.Errors, ErrorNode {
-			Err: err, 
-			Request: nil, 
+		errChan.Errors = append(errChan.Errors, ErrorNode{
+			Err:     err,
+			Request: nil,
 		})
 
-		return errChan   
-	} 
+		return errChan
+	}
 
-	conceptBuf, err := json.Marshal(conceptJson); 
+	conceptBuf, err := json.Marshal(conceptJson)
 	if err != nil {
-		errChan.Errors = append(errChan.Errors, ErrorNode {
-			Err: err, 
-			Request: nil, 
+		errChan.Errors = append(errChan.Errors, ErrorNode{
+			Err:     err,
+			Request: nil,
 		})
 
-		return errChan   
-	} 
+		return errChan
+	}
 
-	headingBuf, err := json.Marshal(headingJson); 
+	headingBuf, err := json.Marshal(headingJson)
 	if err != nil {
-		errChan.Errors = append(errChan.Errors, ErrorNode {
-			Err: err, 
-			Request: nil, 
+		errChan.Errors = append(errChan.Errors, ErrorNode{
+			Err:     err,
+			Request: nil,
 		})
 
-		return errChan   
-	} 
+		return errChan
+	}
 
-	topicReader := bytes.NewReader(topicBuf); 
-	headingReader := bytes.NewReader(headingBuf); 
-	conceptReader := bytes.NewReader(conceptBuf); 
+	topicReader := bytes.NewReader(topicBuf)
+	headingReader := bytes.NewReader(headingBuf)
+	conceptReader := bytes.NewReader(conceptBuf)
 
 	topicRequest, err := http.NewRequest(
-		http.MethodPost, 
-		url + "/modify_branch", 
-		topicReader, 
-	) 
+		http.MethodPost,
+		url+"/modify_branch",
+		topicReader,
+	)
 	conceptRequest, err := http.NewRequest(
-		http.MethodPost, 
-		url + "/modify_branch", 
-		conceptReader, 
-	) 
+		http.MethodPost,
+		url+"/modify_branch",
+		conceptReader,
+	)
 
 	headingRequest, err := http.NewRequest(
-		http.MethodPost, 
-		url + "/modify_branch", 
-		headingReader, 
-	) 
+		http.MethodPost,
+		url+"/modify_branch",
+		headingReader,
+	)
 
-	requests := [] *http.Request { topicRequest, headingRequest, conceptRequest }; 
-	var wg sync.WaitGroup; 
+	requests := []*http.Request{topicRequest, headingRequest, conceptRequest}
+	var wg sync.WaitGroup
 
 	for _, request := range requests {
-		go func () {
-			defer wg.Done(); 
-			res, err := client.Do(request); 
+		go func() {
+			defer wg.Done()
+			res, err := client.Do(request)
 			if err != nil {
 				errChan.AddErrorNode(ErrorNode{
-					Err: 	err, 
-					Request: request, 
-				}); 
+					Err:     err,
+					Request: request,
+				})
 
-			} 
+			}
 
 			if res.StatusCode != http.StatusOK {
 				errChan.AddErrorNode(ErrorNode{
-					Err: 	fmt.Errorf("error : %s", err.Error()), 
-					Request: request, 
-				}); 
-			} 
-		}()  
-	} 
+					Err:     fmt.Errorf("error : %s", err.Error()),
+					Request: request,
+				})
+			}
+		}()
+	}
 
-	wg.Wait(); 
+	wg.Wait()
 
-	return errChan; 
+	return errChan
 
-} 
+}
